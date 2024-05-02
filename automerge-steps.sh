facf4bb0861c9bb9b8bf6ab5bccd4918b6c6d6df
unawarez <dev@unawarez.com>
2024-05-02 16:18:27 -0500

    clean up automerge; keep keys/ fresh

---
+++ automerge-steps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# keys/ should be completely replaced with upstream every time.
+# automerging it will not necessarily delete old keys.
+git checkout MERGE_HEAD -- keys/
+
+# .SRCINFO needs to be updated with makepkg. to keep this runnable
+# outside of arch, it's just deleted for now, to lower the amount of
+# noise in the diff. TODO if PKGBUILD gets sed'd then this could also
+# be sed'd with the same replacements, to automate it correctly here
+# 99% of the time. also TODO check for makepkg and just do it now,
+# though note "check for makepkg" also means check uid != 0 and files AND cwd
+# are writable.
+git rm -f .SRCINFO
+
+# TODO take PKGBUILD as ours with a couple fields sed'd(?) from theirs.
+# also TODO if updpkgsums is available, run it now.
